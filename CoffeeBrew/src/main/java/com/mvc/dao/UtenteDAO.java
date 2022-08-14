db52a9801addd27ed10af185095eabf82588a994
giorgio windows <giorgio@windows.com>
2022-08-14 18:07:26 +0200

    utente dao

---
+++ CoffeeBrew/src/main/java/com/mvc/dao/UtenteDAO.java
@@ -0,0 +1,43 @@
+package com.mvc.dao;
+
+import com.mvc.bean.UtenteBean;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UtenteDAO {
+	public static boolean registraUtente(UtenteBean registrazioneUtente) {
+		String nome = registrazioneUtente.getNome();
+		String cognome = registrazioneUtente.getCognome();
+		String email = registrazioneUtente.getEmail();
+		String password = registrazioneUtente.getPassword();
+		
+		String DBurl = "sql11.freemysqlhosting.net:3306";
+		String DBusername = "sql11512766";
+		String DBpassword = "z9114hAzpB";
+		
+		try {
+			Connection connection = DriverManager.getConnection(DBurl, DBusername, DBpassword);
+			
+			PreparedStatement pstmt = null;
+			pstmt = connection.prepareStatement("INSERT INTO Utente (nome, cognome, email, password) values (?, ?, ?, ?)");
+			pstmt.setString(1, nome);
+			pstmt.setString(2, cognome);
+			pstmt.setString(3, email);
+			pstmt.setString(4, password);
+			int result = pstmt.executeUpdate();
+			
+			pstmt.close();
+			connection.close();
+			
+			return result > 0;
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+}

5fcd846e44e0a88d5409059564dda30f485eaecf
BicoBaq <vic.messi99@gmail.com>
2022-08-14 18:08:51 +0200

    controller

--- CoffeeBrew/src/main/java/com/mvc/controller/RegistrazioneUtenteController.java
+++ CoffeeBrew/src/main/java/com/mvc/controller/RegistrazioneUtenteController.java
@@ -1,15 +1,18 @@
 package com.mvc.controller;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import com.mvc.bean.UtenteBean;
+import com.mvc.dao.UtenteDAO;
+
 /**
  * Servlet implementation class RegistrazioneUtenteController
  */
+
 public class RegistrazioneUtenteController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -34,7 +37,27 @@ public class RegistrazioneUtenteController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		if(request.getParameter("nome") != null && request.getParameter("nome") != "" &&
+		   request.getParameter("cognome") != null && request.getParameter("cognome") != "" &&
+		   request.getParameter("email") != null && request.getParameter("email") != "" &&
+		   request.getParameter("password") != null && request.getParameter("password") != "") {
+			
+			String nome = request.getParameter("nome");
+			String cognome = request.getParameter("cognome");
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");
+			
+			UtenteBean registrazioneUtente = new UtenteBean();
+			registrazioneUtente.setNome(nome);
+			registrazioneUtente.setCognome(cognome);
+			registrazioneUtente.setEmail(email);
+			registrazioneUtente.setPassword(password);
+			
+			//TODO UtenteDAO
+			boolean result;
+			result = UtenteDAO.registraUtente(registrazioneUtente);
+			response.getWriter().print("Utente "+email+" registrato: "+result);
+		}
 	}
 
 }

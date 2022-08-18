28c3cf5f3e03cca1d3d4c55b48a106421715b3f8
BicoBaq <vic.messi99@gmail.com>
2022-08-18 13:20:16 +0200

    ZIOPEWRA

--- CoffeeBrew/src/main/java/com/mvc/dao/DistributoreAutomaticoDAO.java
+++ CoffeeBrew/src/main/java/com/mvc/dao/DistributoreAutomaticoDAO.java
@@ -45,33 +45,4 @@ public class DistributoreAutomaticoDAO {
 		
 		return false;
 	}
-	
-	public static boolean registraUtente(UtenteBean registrazioneUtente) {
-		String nome = registrazioneUtente.getNome();
-		String cognome = registrazioneUtente.getCognome();
-		String email = registrazioneUtente.getEmail();
-		String password = registrazioneUtente.getPassword();
-		
-		try {
-			Connection connection = DBHelper.connectToDB();
-			
-			PreparedStatement pstmt = null;
-			pstmt = connection.prepareStatement("INSERT INTO Utente (nome, cognome, email, password) values (?, ?, ?, ?)");
-			pstmt.setString(1, nome);
-			pstmt.setString(2, cognome);
-			pstmt.setString(3, email);
-			pstmt.setString(4, DBHelper.getSHA512(password));
-			int result = pstmt.executeUpdate();
-			
-			pstmt.close();
-			connection.close();
-			
-			return result > 0;
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-		return false; 
-	}
 }

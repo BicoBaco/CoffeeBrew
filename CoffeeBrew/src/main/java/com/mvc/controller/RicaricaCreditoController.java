965ce8e441fada8300463f742ac26e7d62330d50
giorgio windows <giorgio@windows.com>
2022-08-19 11:18:12 +0200

    Merge branch 'master' of https://github.com/Atovange/CoffeeBrew.git

--- CoffeeBrew/src/main/java/com/mvc/controller/RicaricaCreditoController.java
+++ CoffeeBrew/src/main/java/com/mvc/controller/RicaricaCreditoController.java
@@ -36,6 +36,7 @@ public class RicaricaCreditoController extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		if(request.getSession().getAttribute("utente") != null) {

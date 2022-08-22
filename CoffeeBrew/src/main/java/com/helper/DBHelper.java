ef3d22430bf59a41e753093dc16e5546a2c5490e
BicoBaq <vic.messi99@gmail.com>
2022-08-22 10:48:48 +0200

    db

--- CoffeeBrew/src/main/java/com/helper/DBHelper.java
+++ CoffeeBrew/src/main/java/com/helper/DBHelper.java
@@ -7,9 +7,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBHelper {
-	private static String DBurl = "jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11512766";
-	private static String DBusername = "sql11512766";
-	private static String DBpassword = "z9114hAzpB";
+	private static String DBurl = "jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11514315";
+	private static String DBusername = "sql11514315";
+	private static String DBpassword = "TtCwjiAQF7";
 	
 	public static Connection connectToDB() throws SQLException, ClassNotFoundException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
